84d772b299e38469f21c797190c173d8d759e217
SachinBarpanda <sachinongithub@gmail.com>
2021-07-04 00:09:18 +0530

    Ran JFrame

---
+++ src/com/firstProject/Game.java
@@ -0,0 +1,16 @@
+package com.firstProject;
+
+
+import javax.swing.*;
+
+public class Game {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Program");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
